044cd3b452fff782cd2d448a51b476a41bf5e05e
carloslevi <carlos2levi@gmail.com>
2026-05-25 18:51:37 -0300

    Classe filme devidamente preenchida

--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,20 @@
 public class Filme {
+    int id;
+    String titulo;
+    String genero;
+
+    public Filme(int id, String titulo, String genero) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
 }
